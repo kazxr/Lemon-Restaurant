93bd36768554303a6f0851cc35d1b48d8d1a5d68
Youssef <koxixr@gmail.com>
2025-01-18 23:40:31 +0100

    projects-base-stracture

---
+++ README.md
@@ -0,0 +1,12 @@
+
+pre made set-up :
+1-git
+2-taildwind
+3-routing
+4-formik
+5-yup
+6-chakraui(not installed)
+7-vitest
+
+and html index OGP and SEO;
+
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>hellooo</h1>
+      
+    </>
+  )
+}
+
+export default App
+
+
