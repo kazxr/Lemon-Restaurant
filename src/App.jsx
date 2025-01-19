af933d7f287aec5bfa8db6751bd4ea25062a38fe
Youssef <koxixr@gmail.com>
2025-01-19 20:47:05 +0100

    projects-base-stracture

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
-
   return (
     <>
-    <h1>hellooo</h1>
-      
+      <h1 id="hoho" className=" ff text-blue-700 w-56 bg-slate-700 ">heloo</h1>
+      <h2 className="bg-gray-700 text">Lorem, ipsum dolor.</h2>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,23 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      //   colors: {
+      //   greenPrimary: "#495E57", 
+      //   yellowPrimary: "#F4CE14", 
+      //   orangeSecondary: "#EE9972",
+      //   lightOrangeSecondary: "#FBDABB",
+      //   graySecondary: "#EDEFEE",
+      //   blackSecondary: "#333333",
+
+
+
+      // },
+      // fontFamily: {
+      //    markazi: ['"Markazi Text"', "serif"], // Markazi Text Medium
+      //    karla: ['"Karla"', "sans-serif"], // karla regular
+      // },
+    },
   },
   plugins: [],
 }
-
